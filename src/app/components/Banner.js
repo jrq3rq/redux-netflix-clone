9f6dd44e0648b10354d7378bc9047f8d50fe653b
YOUR NAME <YOUR EMAIL ADDRESS>
2021-02-16 14:54:14 -0800

    added row component, axios request to tmdb api

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -1,7 +1,29 @@
 import React from "react";
 import "./Banner.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { string } from "prop-types";
+import axios from "../axios";
+import requests from "../Requests";
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
@@ -10,21 +32,20 @@ const Banner = () => {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">movie name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My list</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            `test description here! test description here!test description here!test description here!test description here!test description here!test description here!test description here!test description here!test description here!test description here!test description here!test description here!test description here!test description here!test description here!`,
-            150
-          )}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
       <div className="banner--fadeButton" />
@@ -33,5 +54,3 @@ const Banner = () => {
 };
 
 export default Banner;
-
-// Truncation
